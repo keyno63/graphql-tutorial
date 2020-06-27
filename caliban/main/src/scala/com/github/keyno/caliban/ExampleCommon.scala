aa112a25d11159370538d6d02d13c98c1792c436
Keyno <kofujiw@gmail.com>
2020-06-27 12:57:22 +0900

    とりあえずデータとAppを分割した

---
+++ caliban/main/src/scala/com/github/keyno/caliban/ExampleCommon.scala
@@ -0,0 +1,158 @@
+package com.github.keyno.caliban
+
+import caliban.{GraphQL, RootResolver}
+import caliban.GraphQL.graphQL
+import caliban.schema.Annotations.{GQLDeprecated, GQLDescription}
+import caliban.schema.GenericSchema
+import caliban.wrappers.ApolloTracing.apolloTracing
+import caliban.wrappers.Wrappers.{maxDepth, maxFields, printSlowQueries, timeout}
+import com.github.keyno.caliban.ExampleData.{Character, CharacterArgs, CharactersArgs, Origin, Role}
+import zio.{Has, Queue, Ref, UIO, URIO, ZLayer}
+import zio.clock.Clock
+import zio.console.Console
+import zio.stream.ZStream
+import com.github.keyno.caliban.ExampleData.Origin._
+import com.github.keyno.caliban.ExampleData.Role._
+import com.github.keyno.caliban.ExampleService.ExampleService
+import zio.duration._
+import zio.stream.ZStream
+
+object ExampleData {
+
+  sealed trait Origin
+
+  object Origin {
+    case object EARTH extends Origin
+    case object MARS  extends Origin
+    case object BELT  extends Origin
+  }
+
+  sealed trait Role
+
+  object Role {
+    case class Captain(shipName: String)  extends Role
+    case class Pilot(shipName: String)    extends Role
+    case class Engineer(shipName: String) extends Role
+    case class Mechanic(shipName: String) extends Role
+  }
+
+  case class Character(name: String, nicknames: List[String], origin: Origin, role: Option[Role])
+
+  case class CharactersArgs(origin: Option[Origin])
+  case class CharacterArgs(name: String)
+
+  val sampleCharacters = List(
+    Character("James Holden", List("Jim", "Hoss"), EARTH, Some(Captain("Rocinante"))),
+    Character("Naomi Nagata", Nil, BELT, Some(Engineer("Rocinante"))),
+    Character("Amos Burton", Nil, EARTH, Some(Mechanic("Rocinante"))),
+    Character("Alex Kamal", Nil, MARS, Some(Pilot("Rocinante"))),
+    Character("Chrisjen Avasarala", Nil, EARTH, None),
+    Character("Josephus Miller", List("Joe"), BELT, None),
+    Character("Roberta Draper", List("Bobbie", "Gunny"), MARS, None)
+  )
+}
+
+object ExampleService {
+
+  type ExampleService = Has[Service]
+
+  trait Service {
+    def getCharacters(origin: Option[Origin]): UIO[List[Character]]
+
+    def findCharacter(name: String): UIO[Option[Character]]
+
+    def deleteCharacter(name: String): UIO[Boolean]
+
+    def deletedEvents: ZStream[Any, Nothing, String]
+  }
+
+  def getCharacters(origin: Option[Origin]): URIO[ExampleService, List[Character]] =
+    URIO.accessM(_.get.getCharacters(origin))
+
+  def findCharacter(name: String): URIO[ExampleService, Option[Character]] =
+    URIO.accessM(_.get.findCharacter(name))
+
+  def deleteCharacter(name: String): URIO[ExampleService, Boolean] =
+    URIO.accessM(_.get.deleteCharacter(name))
+
+  def deletedEvents: ZStream[ExampleService, Nothing, String] =
+    ZStream.accessStream(_.get.deletedEvents)
+
+  def make(initial: List[Character]): ZLayer[Any, Nothing, ExampleService] = ZLayer.fromEffect {
+    for {
+      characters  <- Ref.make(initial)
+      subscribers <- Ref.make(List.empty[Queue[String]])
+    } yield new Service {
+
+      def getCharacters(origin: Option[Origin]): UIO[List[Character]] =
+        characters.get.map(_.filter(c => origin.forall(c.origin == _)))
+
+      def findCharacter(name: String): UIO[Option[Character]] = characters.get.map(_.find(c => c.name == name))
+
+      def deleteCharacter(name: String): UIO[Boolean] =
+        characters
+          .modify(list =>
+            if (list.exists(_.name == name)) (true, list.filterNot(_.name == name))
+            else (false, list)
+          )
+          .tap(deleted =>
+            UIO.when(deleted)(
+              subscribers.get.flatMap(
+                // add item to all subscribers
+                UIO.foreach(_)(queue =>
+                  queue
+                    .offer(name)
+                    .catchSomeCause {
+                      case cause if cause.interrupted =>
+                        subscribers.update(_.filterNot(_ == queue)).as(false)
+                    } // if queue was shutdown, remove from subscribers
+                )
+              )
+            )
+          )
+
+      def deletedEvents: ZStream[Any, Nothing, String] = ZStream.unwrap {
+        for {
+          queue <- Queue.unbounded[String]
+          _     <- subscribers.update(queue :: _)
+        } yield ZStream.fromQueue(queue).ensuring(queue.shutdown)
+      }
+    }
+  }
+}
+
+
+object ExampleApi extends GenericSchema[ExampleService] {
+
+  case class Queries(
+                      @GQLDescription("Return all characters from a given origin")
+                      characters: CharactersArgs => URIO[ExampleService, List[Character]],
+                      @GQLDeprecated("Use `characters`")
+                      character: CharacterArgs => URIO[ExampleService, Option[Character]]
+                    )
+  case class Mutations(deleteCharacter: CharacterArgs => URIO[ExampleService, Boolean])
+  case class Subscriptions(characterDeleted: ZStream[ExampleService, Nothing, String])
+
+  implicit val roleSchema           = gen[Role]
+  implicit val characterSchema      = gen[Character]
+  implicit val characterArgsSchema  = gen[CharacterArgs]
+  implicit val charactersArgsSchema = gen[CharactersArgs]
+
+  val api: GraphQL[Console with Clock with ExampleService] =
+    graphQL(
+      RootResolver(
+        Queries(
+          args => ExampleService.getCharacters(args.origin),
+          args => ExampleService.findCharacter(args.name)
+        ),
+        Mutations(args => ExampleService.deleteCharacter(args.name)),
+        Subscriptions(ExampleService.deletedEvents)
+      )
+    ) @@
+      maxFields(200) @@               // query analyzer that limit query fields
+      maxDepth(30) @@                 // query analyzer that limit query depth
+      timeout(3 seconds) @@           // wrapper that fails slow queries
+      printSlowQueries(500 millis) @@ // wrapper that logs slow queries
+      apolloTracing // wrapper for https://github.com/apollographql/apollo-tracing
+
+}
